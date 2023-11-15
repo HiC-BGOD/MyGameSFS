83055862262548afad42b02e5d9516cab3e19990
Blane Anderson <143014086+HiC-BGOD@users.noreply.github.com>
2023-11-15 18:54:21 -0500

    Complete

--- Player/Attacking.gd
+++ Player/Attacking.gd
@@ -2,13 +2,16 @@ extends Node
 
 @onready var SM = get_parent()
 @onready var player = get_node("../..")
+var is_attacking = false
 
 func _ready():
 	await player.ready
 
 func start():
-	player.set_animation("Attack1",Vector2(5,7))
-	$Timer.start()
+	if !is_attacking:
+		is_attacking = true
+		player.set_animation("Attack1",Vector2(5,7))
+		$Timer.start()
 
 func end():
 	player.get_node("AnimatedSprite2D").offset.y = 0
@@ -21,4 +24,5 @@ func physics_process(_delta):
 func _on_timer_timeout():
 	if SM.state_name == "Attacking":
 		player.attack()
+		is_attacking = false
 		
--- Player/Bosses/Attack.gd
+++ Player/Bosses/Attack.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var SM = get_parent()
 @onready var enemy = get_node("../..")
 
+
 func _ready():
 	await enemy.ready
 
